9e5c7dc6eea0f8016b751e4bd70308da12dcb6c3
Guilherme Tamanini <guilherme.gui.tama@gmail.com>
2023-05-26 14:20:53 -0300

    Computer model creation

---
+++ inforratica/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Computer(models.Model):
+    placa_mae = models.CharField(max_length=100)
+    processador = models.CharField(max_length=100)
+    memoria = models.CharField(max_length=100)
+    hd = models.CharField(max_length=100)
+    ssd = models.CharField(max_length=100)
+    fonte = models.CharField(max_length=100)
+    gabinete = models.CharField(max_length=100)
+    placa_video = models.CharField(max_length=100)
+    preco = models.CharField(max_length=100)
+    imagem = models.CharField(max_length=100)
+    def __str__(self):
+        return self.placa_mae
