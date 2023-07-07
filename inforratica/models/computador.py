1cdfd60ead3746cae29443429fd5d428d6f71c93
FelipeRotermel <feliperotermell@gmail.com>
2023-07-07 20:21:14 -0300

    Computador terminado

--- inforratica/models/computador.py
+++ inforratica/models/computador.py
@@ -1,16 +1,15 @@
 from django.db import models
 
 class Computador(models.Model):
-    cliente = models.CharField(max_length=100)
-    placa_mae = models.CharField(max_length=100)
-    processador = models.CharField(max_length=100)
-    memoria_ram = models.CharField(max_length=100)
-    hd = models.CharField(max_length=100)
-    ssd = models.CharField(max_length=100)
-    fonte = models.CharField(max_length=100)
-    gabinete = models.CharField(max_length=100)
-    placa_de_video = models.CharField(max_length=100)
-    preco = models.CharField(max_length=100)
-    imagem = models.URLField(max_length=100)
+    placa_mae = models.CharField(max_length=50)
+    processador = models.CharField(max_length=50)
+    memoria_ram = models.CharField(max_length=50)
+    cooler = models.CharField(max_length=50, default="Stock")
+    hd = models.CharField(max_length=50)
+    ssd = models.CharField(max_length=50)
+    fonte = models.CharField(max_length=50)
+    gabinete = models.CharField(max_length=50)
+    placa_de_video = models.CharField(max_length=50)
+    imagem = models.URLField(max_length=200)
     def __str__(self):
-        return f"{self.cliente} ({self.id})"
+        return f"{self.gabinete}"
