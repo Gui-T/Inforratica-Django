7d499f04b5ed13f7ab7ebce8f7a7e6ef737af304
Felipe Rotermel <feliperotermell@gmail.com>
2023-07-28 14:57:57 -0300

    verificação de CPF comentado

--- inforratica/models/cliente.py
+++ inforratica/models/cliente.py
@@ -1,9 +1,8 @@
 from django.db import models
-from cpf_field.models import CPFField
 
 class Cliente(models.Model):
     nome = models.CharField(max_length=50)
-    cpf = CPFField(max_length=11)
+    cpf = models.CharField(max_length=11)
     email = models.EmailField(max_length=254)
     telefone = models.CharField(max_length=50)
     endereco = models.CharField(max_length=50)
