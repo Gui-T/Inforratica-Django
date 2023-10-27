7d3592a9c0917b3e0d7441de0a1443ef62ec3f48
Felipe Rotermel <feliperotermell@gmail.com>
2023-10-27 14:41:41 -0300

    depth

--- inforratica/serializers/ordemservico.py
+++ inforratica/serializers/ordemservico.py
@@ -25,10 +25,12 @@ class OrdemServicoReadSerializer(ModelSerializer):
     class Meta:
         model = OrdemServico
         fields = "__all__"
+        depth = 1
 
 
 class OrdemServicoSerializer(serializers.ModelSerializer):
     class Meta:
         model = OrdemServico
         fields = "__all__"
+        depth = 1
         
